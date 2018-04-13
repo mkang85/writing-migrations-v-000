3abbcdd8790a9a6f8227dde8825a11f1f01137db
Michael Kang <michaelkang85@gmail.com>
2018-04-13 00:39:35 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeForBirthdate
-  def change 
-    change_column(students, birthdate, datetime)
-  end 
+  def change
+    change_column(:students, :birthdate, :datetime)
+  end
 end
